b23707e7465e7f4f713a67a83c9b3afaf6def969
Jeremy Langevin <langevinj832@gmail.com>
2020-11-06 07:50:36 -0500

    unit test for partialUpdate helper function

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,21 @@
+const { it, expect } = require("@jest/globals");
+const sqlForPartialUpdate = require("/Users/JeremyLangevin/Springboard/Unit_37/express-jobly/helpers/partialUpdate");
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
-      function () {
-
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
-
-  });
+    function () {
+      const resp = sqlForPartialUpdate("testTable", {"count": 5}, "handle", 123)
+      // FIXME: write real tests!
+      expect(resp.query).toEqual(`UPDATE testTable SET count=$1 WHERE handle=$2 RETURNING *`);
+      expect(resp.values).toEqual([5, 123])
+    });
+    
+  it("should correctly increment variable identifiers with multiple fields",
+    function(){
+      const resp = sqlForPartialUpdate("testTable", { "count": 5, "name": "Test"}, "handle", 123)
+      // FIXME: write real tests!
+      expect(resp.query).toEqual(`UPDATE testTable SET count=$1, name=$2 WHERE handle=$3 RETURNING *`);
+      expect(resp.values).toEqual([5, "Test", 123])
+    })
 });
+
