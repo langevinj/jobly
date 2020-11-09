5b58edc26a326673e994d31951643727477a7ed3
Jeremy Langevin <langevinj832@gmail.com>
2020-11-09 07:26:07 -0500

    testing for companies GET / route written

--- __tests__/integration/companiesRoutes.test.js
+++ __tests__/integration/companiesRoutes.test.js
@@ -26,22 +26,91 @@ describe("Company Routes Test", function () {
         });
     });
 
-    /** GET /companies => [{company}, ...] */
+    //add a second company when needed for testing
+    async function addSecondCompany(){
+        let c2 = await Company.create({
+            handle: "ibm",
+            name: "IBM",
+            num_employees: 1000,
+            description: "IT services",
+            logo_url: "www.ibm.com"
+        });
+    }
 
-    test("can get a list containing 1 book", async function(){
-        let response = await request(app)
-            .get("/companies")
-        
-        expect(response.body).toEqual({
-            "companies": [{
-                "handle": "apple",
-                "name": "Apple",
-                "num_employees": 4000,
-                "description": "Tech and computers",
-                "logo_url": "www.apple.com"
-            }]
+    /** GET /companies => [{handle, name}, ...] */
+    describe("GET /", function () {
+        test("can get a list containing 1 company", async function () {
+            let response = await request(app)
+                .get("/companies")
+
+            expect(response.body).toEqual({
+                "companies": [{
+                    "handle": "apple",
+                    "name": "Apple"
+                }]
+            });
+        });
+
+        test("can get a filtered list of companies by a searched name", async function(){
+            await addSecondCompany();
+            let response = await request(app)
+                .get("/companies?search=apple")
+            
+            expect(response.body).toEqual({
+                "companies": [{
+                    "handle": "apple",
+                    "name": "Apple"
+                }]
+            });
+        });
+
+        test("can get a filter list of companies by min_employees", async function() {
+            await addSecondCompany();
+            let response = await request(app)
+                .get("/companies?min_employees=2000")
+
+            expect(response.body).toEqual({
+                "companies": [{
+                    "handle": "apple",
+                    "name": "Apple"
+                }]
+            });
+        });
+
+        test("can get a filter list of companies by max_employees", async function () {
+            await addSecondCompany();
+            let response = await request(app)
+                .get("/companies?max_employees=2000")
+
+            expect(response.body).toEqual({
+                "companies": [{
+                    "handle": "ibm",
+                    "name": "IBM"
+                }]
+            });
+        });
+
+        test("can filter companies by multiple parameters", async function() {
+            await addSecondCompany();
+            let response = await request(app)
+                .get("/companies?search=apple&min_employees=1500&max_employees=9000")
+
+            expect(response.body).toEqual({
+                "companies": [{
+                    "handle": "apple",
+                    "name": "Apple"
+                }]
+            });
+        });
+
+        test("400 error if min_employees is higher than max_employees", async function(){
+            let response = await request(app)
+                .get("/companies?min_employees=6000&max_employees=5000")
+
+            expect(response.statusCode).toEqual(400)
         });
     });
+    
 
 
 });
